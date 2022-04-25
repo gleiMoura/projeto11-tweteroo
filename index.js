2a566f3dfc86879fab6f87ff827c0257fcc71e61
Gleison Moura <gle.bar.mou@gmail.com>
2022-04-24 21:38:10 -0300

    put endpoint with query string to choose the page of tweets

--- index.js
+++ index.js
@@ -15,20 +15,28 @@ app.listen(port, () => {
 
 app.post('/sign-up', (req, res) => {
     let user = req.body;
-    if (users.length === 0) {
-        users.push(user);
-    } else {
-        users.forEach(element => {
-            if (user.username !== element.username) {
+    if (Object.keys(user).length === 2 && Object.keys(user)[0] === "username" && Object.keys(user)[1] === "avatar") {
+        if (user.username === '' || user.avatar === '') {
+            res.status(400).send('Todos os campos são obrigatórios!');
+        } else {
+            if (users.length === 0) {
                 users.push(user);
+            } else {
+                users.forEach(element => {
+                    if (user.username !== element.username) { //não coloca um usuário mais de uma vez;
+                        users.push(user);
+                    }
+                })
             }
-        })
+            res.status(201).send(user)
+        }
+    } else {
+        res.sendStatus(400);
     }
-    res.send("OK");
 })
 
 app.get('/sign-up', (req, res) => {
-    res.send(users)
+    res.send(users);
 })
 
 app.post('/tweets', (req, res) => {
@@ -45,19 +53,48 @@ app.post('/tweets', (req, res) => {
         avatar: avatar,
         tweet: req.body.tweet
     };
-    tweets.push(tweet);
-    res.send("OK");
+    if (tweet.username === '' || tweet.tweet === '') {
+        res.status(400).send('Todos os campos são obrigatórios!')
+    } else {
+        tweets.push(tweet);
+        res.status(201).send(tweet);
+    }
+});
+
+//app.get('/tweets', (req, res) => {
+//    const tenTweets = [];
+//    if (tweets.length > 10) {
+//       for (let i = tweets.length - 1; i > tweets.length - 11; i--) {
+//           tenTweets.push(tweets[i]);
+//        }
+//        res.send(tenTweets)
+//    } else {
+//       res.send(tweets);
+//    }
+//});
+
+app.get('/tweets/:user', (req, res) => {
+    const person = req.params.user;
+    if (tweets.length !== 0) {
+        const personTweets = tweets.filter(element => {
+            if (element.username === person) {
+                return element;
+            }
+        })
+        res.status(201).send(personTweets);
+    } else {
+
+    }
 });
 
 app.get('/tweets', (req, res) => {
     const tenTweets = [];
-    if (tweets.length > 10) {
-        for (let i = tweets.length - 1; i > tweets.length - 11; i--) {
-            tenTweets.push(tweets[i]);
+    if (tweets.length > req.query.page * 10) {
+       for (let i = tweets.length - 1; i > tweets.length - (req.query.page*10 + 1); i--) {
+           tenTweets.push(tweets[i]);
         }
         res.send(tenTweets)
     } else {
-        res.send(tweets);
+       res.send(tweets);
     }
-})
-
+});
